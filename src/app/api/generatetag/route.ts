3f21f63fbb6b05c054ccfa8a552241d3bf71417b
vikramsamak <vikramsamak02@gmail.com>
2025-09-12 20:05:29 +0530

    refactor : use makeApiRequest utility function to make webhook call for tags generation.

--- src/app/api/generatetag/route.ts
+++ src/app/api/generatetag/route.ts
@@ -1,8 +1,8 @@
-import { ApiResponse } from "@/types/Types";
 import { NextRequest, NextResponse } from "next/server";
-import axios from "axios";
+import { makeApiRequest } from "@/utils";
+import { ApiError } from "@/types";
 
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
     const body = await req.json();
     const { title } = body;
@@ -11,18 +11,23 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: "Title is required" }, { status: 400 });
     }
 
-    const res: ApiResponse = await axios.get(
-      process.env.YT_TAG_API_URL as string,
-      {
-        params: { title },
-      }
-    );
+    const res = await makeApiRequest<{ tags: string[] }>({
+      method: "POST",
+      url: process.env.NEXT_PUBLIC_API_GENERATE_TAGS_WEBHOOK as string,
+      data: { title },
+    });
 
-    return NextResponse.json({ tags: res.data.tags });
-  } catch (error) {
-    console.error(error);
+    return NextResponse.json({ tags: res });
+  } catch (error: ApiError | unknown) {
+    const apiError = error as ApiError;
+    if (apiError && apiError.message) {
+      return NextResponse.json(
+        { error: apiError.message },
+        { status: apiError.status || 500 }
+      );
+    }
     return NextResponse.json(
-      { error: "Internal server error" },
+      { error: "Internal Server Error" },
       { status: 500 }
     );
   }
