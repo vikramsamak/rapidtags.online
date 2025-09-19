c6759b133f49575c329ef8b60297f89765d05ea2
vikramsamak <vikramsamak02@gmail.com>
2025-09-19 22:46:13 +0530

    feat: implement POST endpoint for generating hashtags

--- src/app/api/generate-hashtags/route.ts
+++ src/app/api/generate-hashtags/route.ts
@@ -5,29 +5,33 @@ import { ApiError } from "@/types";
 export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
     const body = await req.json();
-    const { title } = body;
 
-    if (!title) {
-      return NextResponse.json({ error: "Title is required" }, { status: 400 });
+    const { title, platform } = body;
+
+    if (!title || !platform) {
+      return NextResponse.json(
+        { message: "Title and platform are required" },
+        { status: 400 }
+      );
     }
 
-    const res = await makeApiRequest<{ tags: string[] }>({
+    const response = await makeApiRequest<{ hashtags: string[] }>({
       method: "POST",
-      url: process.env.NEXT_PUBLIC_API_GENERATE_TAGS_WEBHOOK as string,
-      data: { title },
+      url: process.env.GENERATE_HASHTAGS_WEBHOOK as string,
+      data: { title, platform },
     });
 
-    return NextResponse.json({ tags: res });
+    return NextResponse.json({ data: response.hashtags }, { status: 200 });
   } catch (error: ApiError | unknown) {
     const apiError = error as ApiError;
     if (apiError && apiError.message) {
       return NextResponse.json(
-        { error: apiError.message },
+        { message: apiError.message },
         { status: apiError.status || 500 }
       );
     }
     return NextResponse.json(
-      { error: "Internal Server Error" },
+      { message: "Internal Server Error" },
       { status: 500 }
     );
   }
