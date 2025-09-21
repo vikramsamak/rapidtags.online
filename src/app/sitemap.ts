fc46214536123cfa6d03ad3e90c982cab8869624
vikramsamak <vikramsamak02@gmail.com>
2025-09-21 10:59:41 +0530

    refactor: simplify lastModified assignment in sitemap function

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,10 +2,17 @@ import { APP_URL } from "@/constants";
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const lastModified = new Date().toISOString();
   return [
     {
       url: APP_URL,
-      lastModified: new Date().toISOString(),
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 1.0,
+    },
+    {
+      url: `${APP_URL}/`,
+      lastModified,
       changeFrequency: "weekly",
       priority: 1.0,
     },
