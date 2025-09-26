1f1f9b6ef4f13a866e10b3d8e267069ee260f5a1
vikramsamak <vikramsamak02@gmail.com>
2025-09-26 20:15:46 +0530

    feat: add experimental CSS optimization settings in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  experimental: {
+    optimizeCss: true,
+    inlineCss: true,
+  },
 };
 
 export default nextConfig;
