b9de59a46fdc22b7c613bc3ebe1e56940e02ee03
vikramsamak <vikramsamak02@gmail.com>
2025-10-12 07:52:50 +0530

    fix : app constants.

--- src/constants/app.constants.ts
+++ src/constants/app.constants.ts
@@ -1,7 +1,8 @@
 export const METADATA = {
-  title: "Rapidtags | Hashtag Generator for YouTube, TikTok, Instagram & More",
+  title: "Rapidtags.online | AI-Powered Hashtag Generator",
+  applicationName: "Rapidtags.online",
   description:
-    "Rapidtags is a free hashtag generator that helps you find the best hashtags for your social media posts. Boost your visibility on platforms like YouTube, TikTok, Instagram, LinkedIn, and Twitter.",
+    "Rapidtags.online is a free, AI-powered hashtag generator that helps you find the best hashtags for your social media posts. Boost your visibility on platforms like YouTube, TikTok, Instagram, LinkedIn, and Twitter.",
   keywords: [
     "hashtag generator",
     "youtube hashtag generator",
@@ -14,9 +15,11 @@ export const METADATA = {
     "keyword research",
     "trending hashtags",
     "rapidtags",
+    "rapidtags",
+    "rapidtags.online",
   ],
   creator: "Vikram Samak",
   authors: [{ name: "Vikram Samak", url: "https://vikramsamak.com" }],
 };
 
-export const APP_URL = "https://www.rapidtags.online";
+export const APP_URL = "https://www.rapitags.online";
