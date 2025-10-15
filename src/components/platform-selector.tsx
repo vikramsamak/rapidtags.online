c7916cec575971a6b91be211adb6e9f6ad0003f2
vikramsamak <vikramsamak02@gmail.com>
2025-10-15 21:51:39 +0530

    feat : add reusable platform selector component.

---
+++ src/components/platform-selector.tsx
@@ -0,0 +1,38 @@
+import { PLATFORM_OPTIONS } from '@/constants';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select';
+import { Dispatch, SetStateAction } from 'react';
+
+interface PlatformSelectorProps {
+  platform: string;
+  setPlatform: Dispatch<SetStateAction<string>>;
+}
+
+export default function PlatformSelector({
+  platform,
+  setPlatform,
+}: PlatformSelectorProps) {
+  return (
+    <Select value={platform} onValueChange={setPlatform}>
+      <SelectTrigger
+        className="bg-background/50 h-12 w-full border-white/10 transition-colors focus:border-white/20"
+        title="Select platform"
+        name="Select platform"
+      >
+        <SelectValue placeholder="Select platform" />
+      </SelectTrigger>
+      <SelectContent>
+        {PLATFORM_OPTIONS.map((option) => (
+          <SelectItem key={option.value} value={option.value}>
+            {option.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
