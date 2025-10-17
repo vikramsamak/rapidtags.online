cc4093955243ffb9ded7b7ded47531a28ad80710
vikramsamak <vikramsamak02@gmail.com>
2025-10-17 08:15:45 +0530

    feat : add label for input.

--- src/components/common-input.tsx
+++ src/components/common-input.tsx
@@ -1,7 +1,9 @@
 import { Input } from './ui/input';
+import { Label } from './ui/label';
 
 interface CommonInputProps {
   id: string;
+  label: string;
   type?: string;
   placeholder: string;
   value: string;
@@ -10,19 +12,25 @@ interface CommonInputProps {
 
 export default function CommonInput({
   id,
+  label,
   type = 'text',
   placeholder,
   value,
   setValue,
 }: CommonInputProps) {
   return (
-    <Input
-      id={id}
-      placeholder={placeholder}
-      type={type}
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-      className="bg-background/50 h-12 border-white/10 transition-colors focus:border-white/20"
-    />
+    <div className="grid w-full items-center gap-1.5">
+      <Label htmlFor={id} className="text-sm font-medium text-white/80">
+        {label}
+      </Label>
+      <Input
+        id={id}
+        placeholder={placeholder}
+        type={type}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="bg-background/50 h-12 border-white/10 transition-colors focus:border-white/20"
+      />
+    </div>
   );
 }
